7fe710a0a4766705f20f12598f63c7a540ea8d38
Jeevadarshan <jeevadarshan11@gmail.com>
2025-11-16 14:34:13 +0530

    V1.0.0.3

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -36,11 +36,39 @@ const Home = () => {
             <br />
             <section className="fScontainer">
                 {/* Feature Preview */}
-                <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
-                    <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-                        src="https://www.canva.com/design/DAG402_BYlU/cMSfco7FCKO80sXxZwL5kg/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-                    </iframe>
+                <div
+                    style={{
+                        position: "relative",
+                        width: "100%",
+                        height: 0,
+                        paddingTop: "56.25%",
+                        paddingBottom: 0,
+                        boxShadow: "0 2px 8px rgba(63,69,81,0.16)",
+                        marginTop: "1.6em",
+                        marginBottom: "0.9em",
+                        overflow: "hidden",
+                        borderRadius: "8px",
+                        willChange: "transform",
+                    }}
+                >
+                    <iframe
+                        loading="lazy"
+                        style={{
+                            position: "absolute",
+                            width: "100%",
+                            height: "100%",
+                            top: 0,
+                            left: 0,
+                            border: "none",
+                            padding: 0,
+                            margin: 0,
+                        }}
+                        src="https://www.canva.com/design/DAG402_BYlU/cMSfco7FCKO80sXxZwL5kg/watch?embed"
+                        allowFullScreen
+                        allow="fullscreen"
+                    ></iframe>
                 </div>
+
             </section>
             <br />
             <br />
